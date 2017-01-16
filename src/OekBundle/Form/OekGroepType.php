fc3c56c3f4757177b0c0cf6164a8f4b3fb718910
Evert van Brussel <ecvanbrussel@gmail.com>
2017-01-16 14:47:14 +0100

    Form types gemaakt voor ticket #67

---
+++ src/OekBundle/Form/OekGroepType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace HsBundle\Form;
+
+use OekBundle\Entity\OekGroep;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Form\AppDateType;
+
+class OekGroepType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('naam');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => OekGroep::class,
+        ]);
+    }
+}
---
+++ src/OekBundle/Form/OekTrainingType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace HsBundle\Form;
+
+use AppBundle\Form\AppTimeType;
+use OekBundle\Entity\OekTraining;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Form\AppDateType;
+
+class OekTrainingType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder->add('naam');
+        $builder->add('locatie');
+        $builder->add('oekGroep');
+        $builder->add('startDatum', AppDateType::class);
+        $builder->add('startTijd', AppTimeType::class);
+        $builder->add('eindDatum', AppDateType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => OekTraining::class,
+        ]);
+    }
+}
