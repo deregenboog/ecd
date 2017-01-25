ba15c1c541825067cee7012f903132901077cf1b
Bart Huttinga <barthuttinga@gmail.com>
2017-01-25 14:33:51 +0100

    standaard schorsingslocatie #21

--- app/controllers/schorsingen_controller.php
+++ app/controllers/schorsingen_controller.php
@@ -126,6 +126,7 @@ class SchorsingenController extends AppController
                 }
             }
         }
+
         if ($klant_id != null) {
             //$redenen = $this->Schorsing->Reden->find('list');
 
@@ -140,8 +141,10 @@ class SchorsingenController extends AppController
             //and the locatie_id field is hidden in the form
             //otherwise we provide a dropdown with locations
             if ($locatie_id != null) {
-                $locatie = $this->Schorsing->Locatie->find('first',
-                    array('conditions' => array('Locatie.id' => $locatie_id), 'recursive' => '-1'));
+                $locatie = $this->Schorsing->Locatie->find('first', array(
+                    'conditions' => array('Locatie.id' => $locatie_id),
+                    'recursive' => '-1',
+                ));
                 $this->set(compact('locatie_id', 'locatie'));
             } else {
                 $this->set('locaties', $this->Schorsing->Locatie->find('list'));
