e12af60980b572349ccc2c395e3c393ac158bf76
Bart Huttinga <barthuttinga@gmail.com>
2017-02-16 15:37:20 +0100

    add form type #124

---
+++ src/OekBundle/Form/OekVerwijzingType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OekBundle\Form;
+
+use OekBundle\Entity\OekGroep;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use OekBundle\Entity\OekVerwijzing;
+
+class OekVerwijzingType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('naam');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => OekVerwijzing::class,
+        ]);
+    }
+}
