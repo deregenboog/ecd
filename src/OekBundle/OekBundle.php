957465fa8323515742c2508200606ad9c59e4092
Bart Huttinga <barthuttinga@gmail.com>
2017-02-16 15:36:40 +0100

    opzet rapportages toevoegen #131

--- src/OekBundle/OekBundle.php
+++ src/OekBundle/OekBundle.php
@@ -3,7 +3,15 @@
 namespace OekBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use OekBundle\DependencyInjection\Compiler\ReportsCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class OekBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ReportsCompilerPass());
+    }
 }
