88fcf24b31ca573f29bce24966d358a0efa1f243
Evert van Brussel <ecvanbrussel@gmail.com>
2017-04-10 12:28:17 +0200

    Added boolean properties to AppBundle Vrijwilliger entity as well

--- src/AppBundle/Entity/Vrijwilliger.php
+++ src/AppBundle/Entity/Vrijwilliger.php
@@ -10,4 +10,72 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Vrijwilliger extends Persoon
 {
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $vogAangevraagd = false;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $vogAanwezig = false;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $overeenkomstAanwezig = false;
+
+    /**
+     * @return bool
+     */
+    public function getVogAangevraagd()
+    {
+        return $this->vogAangevraagd;
+    }
+
+    /**
+     * @param bool $vogAangevraagd
+     * @return Vrijwilliger
+     */
+    public function setVogAangevraagd($vogAangevraagd)
+    {
+        $this->vogAangevraagd = $vogAangevraagd;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getVogAanwezig()
+    {
+        return $this->vogAanwezig;
+    }
+
+    /**
+     * @param bool $vogAanwezig
+     * @return Vrijwilliger
+     */
+    public function setVogAanwezig($vogAanwezig)
+    {
+        $this->vogAanwezig = $vogAanwezig;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getOvereenkomstAanwezig()
+    {
+        return $this->overeenkomstAanwezig;
+    }
+
+    /**
+     * @param bool $overeenkomstAanwezig
+     * @return Vrijwilliger
+     */
+    public function setOvereenkomstAanwezig($overeenkomstAanwezig)
+    {
+        $this->overeenkomstAanwezig = $overeenkomstAanwezig;
+        return $this;
+    }
 }
