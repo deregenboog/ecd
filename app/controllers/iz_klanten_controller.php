caa7526339784917ef6cf7d27f9cf3e4b323de57
Bart Huttinga <barthuttinga@gmail.com>
2017-06-01 11:16:13 +0200

    use Sonata exporter for download #255

--- app/controllers/iz_klanten_controller.php
+++ app/controllers/iz_klanten_controller.php
@@ -8,6 +8,11 @@ use IzBundle\Form\IzKlantSelectType;
 use IzBundle\Service\KlantDaoInterface;
 use IzBundle\Entity\IzKlant;
 use AppBundle\Form\KlantType;
+use Exporter\Writer\XmlExcelWriter;
+use Exporter\Handler;
+use Exporter\Source\IteratorSourceIterator;
+use Exporter\Source\ArraySourceIterator;
+use Exporter\Writer\XlsWriter;
 
 class IzKlantenController extends AppController
 {
@@ -62,17 +67,84 @@ class IzKlantenController extends AppController
     {
         $klanten = $this->klantDao->findAll(null, $filter);
 
-//         $filename = sprintf('iz-deelnemers-%s.csv', (new \DateTime())->format('d-m-Y'));
-//         $this->header('Content-type: text/csv');
-//         $this->header(sprintf('Content-Disposition: attachment; filename="%s";', $filename));
+        $format = 'Y-m-d';
+        $data = [];
+        foreach ($klanten as $izKlant) {
+            /* @var IzKlant $izKlant */
 
-        $filename = sprintf('iz-deelnemers-%s.xls', (new \DateTime())->format('d-m-Y'));
+            $projecten = [];
+            foreach ($izKlant->getIzHulpvragen() as $izHulpvraag) {
+                $projecten[] = $izHulpvraag->getIzProject();
+            }
+
+            $medewerkers = [];
+            foreach ($izKlant->getIzHulpvragen() as $izHulpvraag) {
+                $medewerkers[] = $izHulpvraag->getMedewerker();
+            }
+
+            $data[] = [
+                'Nummer' => $izKlant->getKlant()->getId(),
+                'Intakedatum' => $izKlant->getIzIntake() ? $izKlant->getIzIntake()->getIntakeDatum()->format($format) : null,
+                'Afsluitdatum' => $izKlant->getAfsluitDatum() ? $izKlant->getAfsluitDatum()->format($format) : null,
+                'Geslacht' => $izKlant->getKlant()->getGeslacht(),
+                'Voornaam' => $izKlant->getKlant()->getVoornaam(),
+                'Tussenvoegsel' => $izKlant->getKlant()->getTussenvoegsel(),
+                'Achternaam' => $izKlant->getKlant()->getAchternaam(),
+                'Geboortedatum' => $izKlant->getKlant()->getGeboortedatum() ? $izKlant->getKlant()->getGeboortedatum()->format($format) : null,
+                'E-mail' => $izKlant->getKlant()->getEmail(),
+                'Adres' => $izKlant->getKlant()->getAdres(),
+                'Postcode' => $izKlant->getKlant()->getPostcode(),
+                'Woonplaats' => $izKlant->getKlant()->getPlaats(),
+                'Mobiel' => $izKlant->getKlant()->getMobiel(),
+                'Telefoon' => $izKlant->getKlant()->getTelefoon(),
+                'Werkgebied' => $izKlant->getKlant()->getWerkgebied(),
+                'Gezin met kinderen' => $izKlant->getIzIntake() ? $izKlant->getIzIntake()->isGezinMetKinderen() : null,
+                'Geen post' => $izKlant->getKlant()->getGeenPost(),
+                'Geen e-mail' => $izKlant->getKlant()->getGeenEmail(),
+                'Project(en)' => implode(', ', $projecten),
+                'Medewerker(s)' => implode(', ', $medewerkers),
+                'Gekoppeld' => $izKlant->isGekoppeld() ? 'ja' : 'nee',
+            ];
+        }
+
+        $filename = sprintf('iz-deelnemers-%s.xlsx', (new \DateTime())->format('d-m-Y'));
+        $tmpFilename = sprintf('iz-deelnemers-%s.xlsx', uniqid());
+        $cacheDir = $this->container->getParameter('kernel.cache_dir');
+
+        $source = new ArraySourceIterator($data);
+        $writer = new XmlExcelWriter($cacheDir.'/'.$tmpFilename, true, [
+            'Nummer' => 'Number',
+            'Intakedatum' => 'Date',
+            'Afsluitdatum' => 'Date',
+            'Geslacht' => 'String',
+            'Voornaam' => 'String',
+            'Tussenvoegsel' => 'String',
+            'Achternaam' => 'String',
+            'Geboortedatum' => 'Date',
+            'E-mail' => 'String',
+            'Adres' => 'String',
+            'Postcode' => 'String',
+            'Woonplaats' => 'String',
+            'Mobiel' => 'String',
+            'Telefoon' => 'String',
+            'Werkgebied' => 'String',
+            'Gezin met kinderen' => 'Number',
+            'Geen post' => 'Number',
+            'Geen e-mail' => 'Number',
+            'Project(en)' => 'String',
+        ]);
+        $handler = Handler::create($source, $writer);
+        $handler->export();
+
+        $this->autoRender = false;
         $this->header('Content-type: application/vnd.ms-excel');
         $this->header(sprintf('Content-Disposition: attachment; filename="%s";', $filename));
         $this->header('Content-Transfer-Encoding: binary');
 
-        $this->set('klanten', $klanten);
-        $this->render('download', false);
+        $contents = file_get_contents($cacheDir.'/'.$tmpFilename);
+        unlink($cacheDir.'/'.$tmpFilename);
+
+        return $contents;
     }
 
     public function add($klantId = null)
