9bf6db4e639b727c852e1915c7c280bec9e77a91
Bart Huttinga <barthuttinga@gmail.com>
2017-10-06 11:52:12 +0200

    add zipcode tools #5

---
+++ src/AppBundle/Entity/Postcode.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="postcodes")
+ * @Gedmo\Loggable
+ */
+class Postcode
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="string")
+     * @Gedmo\Versioned
+     */
+    private $postcode;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Gedmo\Versioned
+     */
+    private $stadsdeel;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Gedmo\Versioned
+     */
+    private $postcodegebied;
+
+    public function __construct($postcode, $stadsdeel, $postcodegebied)
+    {
+        $this->postcode = $postcode;
+        $this->stadsdeel = $stadsdeel;
+        $this->postcodegebied = $postcodegebied;
+    }
+
+    public function __toString()
+    {
+        return $this->postcode;
+    }
+
+    public function getPostcode()
+    {
+        return $this->postcode;
+    }
+
+    public function getStadsdeel()
+    {
+        return $this->stadsdeel;
+    }
+
+    public function getPostcodegebied()
+    {
+        return $this->postcodegebied;
+    }
+}
