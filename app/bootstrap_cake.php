fe44f6c44349b728bb1bc6e35def2900ea1103e4
Bart Huttinga <barthuttinga@gmail.com>
2017-12-06 15:47:25 +0100

    add missing bootstrap script

---
+++ app/bootstrap_cake.php
@@ -0,0 +1,19 @@
+<?php
+
+@define('DISABLE_DEFAULT_ERROR_HANDLING', 1);
+@define('DS', DIRECTORY_SEPARATOR);
+@define('ROOT', dirname(dirname(__FILE__)));
+@define('APP_DIR', basename(dirname(__FILE__)));
+@define('CAKE_CORE_INCLUDE_PATH', ROOT);
+
+if (!defined('CORE_PATH')) {
+    if (function_exists('ini_set') && ini_set('include_path', CAKE_CORE_INCLUDE_PATH.PATH_SEPARATOR.ROOT.DS.APP_DIR.DS.PATH_SEPARATOR.ini_get('include_path'))) {
+        define('APP_PATH', null);
+        define('CORE_PATH', null);
+    } else {
+        define('APP_PATH', ROOT.DS.APP_DIR.DS);
+        define('CORE_PATH', CAKE_CORE_INCLUDE_PATH.DS);
+    }
+}
+
+require_once __DIR__.'/../cake/bootstrap.php';
