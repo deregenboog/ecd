531864a06dbdef1b11296f15d88602d17a085656
Bart Huttinga <barthuttinga@gmail.com>
2018-01-02 11:01:36 +0100

    Onderhoudstaak toevoegen die toekomstige afsluitdata verwerkt #420

---
+++ src/HsBundle/Command/KlantUpdateCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HsBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use HsBundle\Entity\Klus;
+use Doctrine\ORM\EntityManager;
+
+class KlantUpdateCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('hs:klant:update');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var $entityManager EntityManager */
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        // find klussen
+        $klussen = $entityManager->getRepository(Klus::class)->createQueryBuilder('klus')
+            ->where('klus.einddatum <= :today')
+            ->andWhere('klus.status != :status_afgerond')
+            ->setParameter('today', new \DateTime('today'))
+            ->setParameter('status_afgerond', Klus::STATUS_AFGEROND)
+            ->getQuery()
+            ->getResult()
+        ;
+
+        foreach ($klussen as $klus) {
+            /* @var $klus Klus */
+            // trigger status update
+            $klus->setEinddatum($klus->getEinddatum());
+            $entityManager->flush($klus);
+        }
+
+        $output->writeln(sprintf('%d klussen bijgewerkt', count($klussen)));
+    }
+}
