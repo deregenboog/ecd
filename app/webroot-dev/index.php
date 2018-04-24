bb0722b01fa297a5556da7f6db52d500f7a430b2
Bart Huttinga <barthuttinga@gmail.com>
2018-04-24 12:11:55 +0200

    Change umask

--- app/webroot-dev/index.php
+++ app/webroot-dev/index.php
@@ -6,10 +6,8 @@ define('WWW_ROOT', dirname(__FILE__).'/');
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
+// https://symfony.com/doc/3.4/setup/file_permissions.html
+umask(0002);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
--- app/webroot/index.php
+++ app/webroot/index.php
@@ -5,6 +5,9 @@ define('WWW_ROOT', dirname(__FILE__).'/');
 
 use Symfony\Component\HttpFoundation\Request;
 
+// https://symfony.com/doc/3.4/setup/file_permissions.html
+umask(0002);
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../autoload.php';
 include_once __DIR__.'/../bootstrap.php.cache';
