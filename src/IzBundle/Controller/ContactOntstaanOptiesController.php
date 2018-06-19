baeca3cf0196e710011ce01aea49967c6ca16912
Bart Huttinga <barthuttinga@gmail.com>
2018-06-19 11:05:36 +0200

    Optie "Contact ontstaan" toevoegen geeft foutmelding #591

--- src/IzBundle/Controller/ContactOntstaanOptiesController.php
+++ src/IzBundle/Controller/ContactOntstaanOptiesController.php
@@ -5,6 +5,7 @@ namespace IzBundle\Controller;
 use AppBundle\Controller\AbstractController;
 use IzBundle\Entity\ContactOntstaan;
 use IzBundle\Form\ContactOntstaanType;
+use IzBundle\Service\ContactOntstaanDaoInterface;
 use JMS\DiExtraBundle\Annotation as DI;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
