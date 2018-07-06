e377f2e488de9fdf2f9e31eb29930a35b23755e5
Bart Huttinga <barthuttinga@gmail.com>
2018-07-06 10:47:26 +0200

    Coördinator koppeling is niet aan te passen #610

--- src/IzBundle/Form/KoppelingType.php
+++ src/IzBundle/Form/KoppelingType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Form\MedewerkerType;
 
 class KoppelingType extends AbstractType
 {
@@ -23,15 +24,35 @@ class KoppelingType extends AbstractType
         $hulpaanbod = $hulpvraag->getKoppeling()->getHulpaanbod();
 
         if ($hulpvraag instanceof Hulpvraag) {
-            $builder->add('hulpvraag', DummyChoiceType::class, [
-                'dummy_label' => (string) $hulpvraag,
+            $builder->add('hulpvraag', HulpvraagType::class, [
+                'inherit_data' => true,
             ]);
+            foreach ($builder->get('hulpvraag')->all() as $elm) {
+                $builder->get('hulpvraag')->remove($elm->getName());
+            }
+            $builder->get('hulpvraag')
+                ->add('deelnemer', DummyChoiceType::class, [
+                    'dummy_label' => (string) $hulpaanbod,
+                ])
+                ->add('medewerker', MedewerkerType::class, [
+                    'label' => 'Coördinator',
+                ])
+            ;
         }
 
         if ($hulpaanbod instanceof Hulpaanbod) {
-            $builder->add('hulpaanbod', DummyChoiceType::class, [
-                'dummy_label' => (string) $hulpaanbod,
-            ]);
+            $builder->add('hulpaanbod', HulpaanbodType::class);
+            foreach ($builder->get('hulpaanbod')->all() as $elm) {
+                $builder->get('hulpaanbod')->remove($elm->getName());
+            }
+            $builder->get('hulpaanbod')
+                ->add('vrijwilliger', DummyChoiceType::class, [
+                    'dummy_label' => (string) $hulpaanbod,
+                ])
+                ->add('medewerker', MedewerkerType::class, [
+                    'label' => 'Coördinator',
+                ])
+            ;
         }
 
         $builder
