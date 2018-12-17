eb88dc55d768843e0611758a36e0bce6914279c4
Bart Huttinga <barthuttinga@gmail.com>
2018-12-17 10:51:02 +0100

    Fix money formatting test

--- tests/HsBundle/Entity/BetalingTest.php
+++ tests/HsBundle/Entity/BetalingTest.php
@@ -4,23 +4,36 @@ namespace Tests\HsBundle\Entity;
 
 use HsBundle\Entity\Betaling;
 use HsBundle\Entity\Factuur;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class BetalingTest extends TestCase
+class BetalingTest extends KernelTestCase
 {
     public function testToString()
     {
+        if (!self::$kernel) {
+            self::bootKernel();
+        }
+
+        // get and set locale
+        $locale = setlocale(LC_ALL, 0);
+        setlocale(LC_ALL, self::$kernel->getContainer()->getParameter('locale'));
+
         $factuur = $this->createMock(Factuur::class);
         $betaling = new Betaling($factuur);
 
         $betaling->setBedrag(123.45);
-        $this->assertEquals('€ 123,45', trim((string) $betaling));
+        $this->assertEquals('€ 123,45', (string) $betaling);
 
         $betaling->setBedrag(123.456789);
-        $this->assertEquals('€ 123,46', trim((string) $betaling));
+        $this->assertEquals('€ 123,46', (string) $betaling);
 
         $betaling->setBedrag(123.454321);
-        $this->assertEquals('€ 123,45', trim((string) $betaling));
+        $this->assertEquals('€ 123,45', (string) $betaling);
+
+        // restore locale
+        if ($locale) {
+            setlocale(LC_ALL, $locale);
+        }
     }
 
     public function testIsDeletable()
