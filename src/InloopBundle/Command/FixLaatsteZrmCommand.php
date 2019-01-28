05c01e2287eb1f05addfa66079897b6e303d91ca
Bart Huttinga <barthuttinga@gmail.com>
2019-01-28 16:06:20 +0100

    Laatste zrm wordt niet bijgewerkt #732

---
+++ src/InloopBundle/Command/FixLaatsteZrmCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace InloopBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FixLaatsteZrmCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('inloop:fix:laatste_zrm');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $manager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        $n = $manager->getConnection()->exec('
+            UPDATE klanten
+            LEFT JOIN (
+                SELECT zrm_reports.klant_id, DATE(MAX(zrm_reports.created)) AS zrm_created
+                FROM zrm_reports
+                GROUP BY zrm_reports.klant_id
+            ) AS zrms ON zrms.klant_id = klanten.id
+            SET klanten.last_zrm = zrms.zrm_created
+            WHERE klanten.last_zrm <> zrms.zrm_created
+        ');
+
+        $output->writeln(sprintf('%d klanten bijgewerkt', $n));
+    }
+}
