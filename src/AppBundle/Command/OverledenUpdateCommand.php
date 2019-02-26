f4542b999afe4ab54f72c3bcd9a04f54da97537f
Bart Huttinga <barthuttinga@gmail.com>
2019-02-26 10:46:52 +0100

    Postblokkade instellen als "overleden" is aangevinkt #14

---
+++ src/AppBundle/Command/OverledenUpdateCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Klant;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class OverledenUpdateCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('app:overleden:update');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /* @var EntityManager $em */
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $klanten = $em->getRepository(Klant::class)->createQueryBuilder('klant')
+            ->where('klant.overleden = TRUE')
+            ->andWhere('klant.geenPost IS NULL OR klant.geenPost = FALSE OR klant.geenEmail IS NULL OR klant.geenEmail = FALSE')
+            ->getQuery()
+            ->getResult();
+
+        $output->writeln('Updating '.count($klanten).' clients...');
+
+        foreach ($klanten as $klant) {
+            /* @var Klant $klant */
+            $klant->setGeenPost(true)->setGeenEmail(true);
+        }
+
+        $em->flush();
+
+        $output->writeln('Finished!');
+    }
+}
