79e71b35d426df912f0f08a77dbd596b5311eee4
Bart Huttinga <barthuttinga@gmail.com>
2019-03-11 15:40:14 +0100

    Ladis-rapportage aanpassen #774

--- app/config/ladis_report.sql
+++ app/config/ladis_report.sql
@@ -1,18 +1,31 @@
 -- START .- Klanten raportage for Ladis
 -- HEAD:  LADIS rapportage
--- FIELDS: id - ECD ID; achternaam - Achternaam; geboortedatum - Geboortedatum; geslacht - Geslacht; BSN - BSN; land - Geboorteland; postcode - Postcode; nationaliteit - Nationaliteit; inkom - Bron van inkomstem; woonsituatie - Woonsituatie; primaire_problematiek - Primaire problematiek; versl - Secundaire problematiek; firstTime - Leeftijd eerste gebruik; periode - Duur problematiek; frequentie - Frequentie gebruik; pgebruik - Wijze van gebruik; vgebruik - Wijze van gebruik secundaire problematiek
+-- FIELDS: id - ECD ID; achternaam - Achternaam; geboortedatum - Geboortedatum; geslacht - Geslacht; BSN - BSN; land - Geboorteland; postcode - Postcode; nationaliteit - Nationaliteit; inkom - Bron van inkomstem; woonsituatie - Woonsituatie; eerste_bezoek - Eerste bezoek; laatste_bezoek - Laatste bezoek; primaire_problematiek - Primaire problematiek; versl - Secundaire problematiek; firstTime - Leeftijd eerste gebruik; periode - Duur problematiek; frequentie - Frequentie gebruik; pgebruik - Wijze van gebruik; vgebruik - Wijze van gebruik secundaire problematiek
 -- ARRAY
 -- !DISABLE
-select k.id, k.achternaam, k.geboortedatum, k.BSN, g.afkorting AS geslacht, l.land, n.naam AS nationaliteit, substr(i.postcode, 1, 4) AS postcode, GROUP_CONCAT(distinct v.naam SEPARATOR ' ') AS versl, GROUP_CONCAT(distinct ink.naam SEPARATOR ' ') AS inkom, w.naam AS woonsituatie, pp.naam AS primaire_problematiek, vp.naam AS periode, vf.naam AS frequentie, GROUP_CONCAT(distinct pg.naam SEPARATOR ' ') AS pgebruik, GROUP_CONCAT(distinct vg.naam SEPARATOR ' ') AS vgebruik, FLOOR(DATEDIFF(i.eerste_gebruik, k.geboortedatum) /365) AS firstTime
+SELECT k.id, k.achternaam, k.geboortedatum, k.BSN, g.afkorting AS geslacht, l.land, n.naam AS nationaliteit, SUBSTR(i.postcode, 1, 4) AS postcode, GROUP_CONCAT(distinct v.naam SEPARATOR ' ') AS versl, GROUP_CONCAT(distinct ink.naam SEPARATOR ' ') AS inkom, w.naam AS woonsituatie, pp.naam AS primaire_problematiek, vp.naam AS periode, vf.naam AS frequentie, GROUP_CONCAT(distinct pg.naam SEPARATOR ' ') AS pgebruik, GROUP_CONCAT(distinct vg.naam SEPARATOR ' ') AS vgebruik, FLOOR(DATEDIFF(i.eerste_gebruik, k.geboortedatum) /365) AS firstTime, DATE(MIN(r.binnen)) AS eerste_bezoek, DATE(MAX(r.binnen)) AS laatste_bezoek
   FROM klanten k
+  INNER JOIN (SELECT * FROM registraties_2010
+      UNION SELECT * FROM registraties_2011
+      UNION SELECT * FROM registraties_2012
+      UNION SELECT * FROM registraties_2013
+      UNION SELECT * FROM registraties) AS r ON (r.klant_id = k.id AND r.binnen BETWEEN :from AND :until)
   LEFT JOIN geslachten g
     ON (k.geslacht_id = g.id)
   LEFT JOIN landen l
     ON (k.land_id = l.id)
   LEFT JOIN nationaliteiten n
     ON (k.nationaliteit_id = n.id)
+  LEFT JOIN (
+    SELECT klant_id, MIN(datum_intake) AS datum_intake
+    FROM intakes
+    WHERE datum_intake BETWEEN :from AND :until
+    GROUP BY klant_id
+  ) eerste_intake
+    ON eerste_intake.klant_id = k.id
   LEFT JOIN intakes i
-    ON (k.laste_intake_id = i.id)
+    ON i.klant_id = k.id
+    AND i.datum_intake = eerste_intake.datum_intake
   LEFT JOIN woonsituaties w
     ON (i.woonsituatie_id = w.id)
   LEFT JOIN verslavingen pp
@@ -37,8 +50,6 @@ select k.id, k.achternaam, k.geboortedatum, k.BSN, g.afkorting AS geslacht, l.la
     ON (int_ver.intake_id = i.id)
   LEFT JOIN verslavingsgebruikswijzen vg
     ON (vg.id = int_ver.verslavingsgebruikswijze_id)
-
- GROUP BY k.id;
-
+  GROUP BY k.id;
 -- LINE BREAK IS NEEDED TO KEEP THE LAST LINE
 
