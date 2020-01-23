6952c722c017c95697fe65e49ef65187da6335d3
J.T. Borger <jan@pinna.nl>
2020-01-23 16:29:13 +0100

    Scip krijgt ErOpUit

---
+++ src/ErOpUitBundle/Event/ScipDeelnemerSubscriber.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ErOpUitBundle\Event;
+
+use ErOpUitBundle\Entity\Klant;
+use ErOpUitBundle\Service\KlantDaoInterface;
+use ScipBundle\Event\Events;
+use ScipBundle\Entity\Deelnemer;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class ScipDeelnemerSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var KlantDaoInterface
+     */
+    private $klantDao;
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            Events::EVENT_DEELNEMER_CREATED => ['addToEropuit'],
+        ];
+    }
+
+    public function __construct(KlantDaoInterface $klantDao)
+    {
+        $this->klantDao = $klantDao;
+    }
+
+    public function addToEropuit(GenericEvent $event)
+    {
+        /* @var $deelnemer Deelnemer */
+        $deelnemer = $event->getSubject();
+
+        // do nothing if not client
+        if (!$deelnemer->getKlant() instanceof \AppBundle\Entity\Klant) {
+            return;
+        }
+
+        $klant = $deelnemer->getKlant();
+
+        // do nothing if already exists
+        $erOpUitDossier = $this->klantDao->findOneByKlant($klant);
+        if ($erOpUitDossier instanceof Klant) {
+            return;
+        }
+
+        $erOpUitDossier = new Klant($klant);
+        $this->klantDao->create($erOpUitDossier);
+    }
+}
--- src/ScipBundle/Event/Events.php
+++ src/ScipBundle/Event/Events.php
@@ -4,4 +4,5 @@ namespace ScipBundle\Event;
 
 class Events
 {
+    const EVENT_DEELNEMER_CREATED = 'scip_deelnemer_created';
 }
