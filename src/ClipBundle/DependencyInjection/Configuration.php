2d174f3b9fafcc7f429159e74ad0133b290bdb4b
Bart Huttinga <barthuttinga@gmail.com>
2020-02-29 13:15:56 +0100

    Pass root to tree builder

--- src/ClipBundle/DependencyInjection/Configuration.php
+++ src/ClipBundle/DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('clip');
+        $treeBuilder = new TreeBuilder('clip');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
