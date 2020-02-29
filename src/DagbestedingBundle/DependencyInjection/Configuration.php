623084d2654aafe224e96b1da846766651d67b97
Bart Huttinga <barthuttinga@gmail.com>
2020-02-29 13:15:56 +0100

    Pass root to tree builder

--- src/DagbestedingBundle/DependencyInjection/Configuration.php
+++ src/DagbestedingBundle/DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('dagbesteding');
+        $treeBuilder = new TreeBuilder('dagbesteding');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
