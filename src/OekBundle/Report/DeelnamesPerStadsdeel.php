8949022e41cdeddbdc63f110c02e451c6a70bcb7
J.T. Borger <jan@pinna.nl>
2020-07-20 15:55:41 +0200

    #920 Deelnames per stadsdeel toegevoegd.

---
+++ src/OekBundle/Report/DeelnamesPerStadsdeel.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace OekBundle\Report;
+
+use AppBundle\Report\AbstractReport;
+use AppBundle\Report\Table;
+use OekBundle\Entity\Deelname;
+use OekBundle\Repository\DeelnameRepository;
+use OekBundle\Repository\TrainingRepository;
+
+class DeelnamesPerStadsdeel extends AbstractReport
+{
+    protected $title = 'Deelnames per stadsdeel';
+
+//    protected $xPath = 'stadsdeel';
+
+    protected $yPath = 'stadsdeel';
+
+    protected $nPath = 'aantal';
+
+    protected $xDescription = 'Aantal deelnemers';
+
+    protected $yDescription = 'Stadsdeel';
+
+    protected $table;
+
+    /**
+     * @var DeelnameRepository
+     */
+    protected $repository;
+
+    public function __construct(DeelnameRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    protected function init()
+    {
+        $this->table = $this->repository->getAantalDeelnamesPerStadsdeel($this->startDate, $this->endDate);
+    }
+
+    protected function build()
+    {
+        $table = new Table($this->table, $this->xPath, $this->yPath, $this->nPath);
+        $table->setStartDate($this->startDate)->setEndDate($this->endDate);
+
+        $this->reports = [
+            [
+                'title' => 'Deelnames per stadsdeel',
+                'xDescription' => $this->xDescription,
+                'yDescription' => $this->yDescription,
+                'data' => $table->render(),
+            ],
+        ];
+    }
+}
---
+++ src/OekBundle/Repository/DeelnameRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace OekBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use OekBundle\Entity\Aanmelding;
+use OekBundle\Entity\Afsluiting;
+use OekBundle\Entity\DeelnameStatus;
+use OekBundle\Report\AbstractDeelnemersVerwezen;
+
+class DeelnameRepository extends EntityRepository
+{
+
+
+    private function getCountBuilder()
+    {
+        return $this->createQueryBuilder('deelname')
+            ->select('COUNT(DISTINCT deelnemer.id) AS aantal')
+            ->innerJoin('deelname.deelnemer', 'deelnemer')
+
+        ;
+    }
+
+
+
+    public function getAantalDeelnamesPerStadsdeel($startdatum,$eindddatum)
+    {
+        $builder = $this->getCountBuilder()
+            ->innerjoin('deelnemer.klant','appKlant')
+            ->innerJoin("appKlant.werkgebied",'werkgebied')
+            ->addSelect('werkgebied.naam AS stadsdeel')
+            ->innerJoin('deelname.deelnameStatussen', 'deelnameStatus')
+            ->innerJoin('deelname.training', 'training')
+            ->innerJoin('training.groep', 'groep')
+//            ->where('deelnameStatus.status = :status')
+            ->andWhere('deelnameStatus.datum BETWEEN :startDate AND :endDate')
+
+//            ->setParameter('status', DeelnameStatus::STATUS_AFGEROND)
+            ->setParameter('startDate', $startdatum)
+            ->setParameter('endDate', $eindddatum)
+
+            ->groupBy('werkgebied.naam')
+        ;
+        return $builder->getQuery()->getResult();
+    }
+    private function getStadsdelen()
+    {
+        $builder =  $this->createQueryBuilder('training')
+            ->select('werkgebied.naam AS werkgebiednaam')
+            ->innerJoin('training.deelnames', 'deelname')
+            ->innerJoin('deelname.deelnemer', 'klant')
+            ->innerJoin('klant.klant', 'appKlant')
+            ->leftJoin('appKlant.werkgebied', 'werkgebied')
+            ->groupBy('werkgebiednaam');
+
+//        $sql = $builder->getQuery()->getSQL();
+//        $parameters = $builder->getQuery()->getParameters();
+        return $builder->getQuery()->getResult();
+    }
+}
