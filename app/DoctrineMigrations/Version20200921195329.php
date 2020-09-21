ac8b1b4d3c8e6b08f0dbd1abe06e3abc26d57931
J.T. Borger <jan@pinna.nl>
2020-09-21 22:02:32 +0200

    Tbv MW overzicht, index.

---
+++ app/DoctrineMigrations/Version20200921195329.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200921195329 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("CREATE INDEX klant_id_med_id ON verslagen(klant_id, medewerker_id)");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("DROO INDEX klant_id_med_id");
+    }
+}
