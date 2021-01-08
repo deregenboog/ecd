93998185f2195e3eb0cbee9e428a9495a52519db
J.T. Borger <jan@pinna.nl>
2021-01-08 12:29:14 +0100

    bump version to v2.2.67

---
+++ app/DoctrineMigrations/Version20201218093703.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201218093703 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+        $this->addSql('ALTER TABLE klanten ADD partner_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE klanten ADD CONSTRAINT FK_F538C5BC9393F8FE FOREIGN KEY (partner_id) REFERENCES klanten (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_F538C5BC9393F8FE ON klanten (partner_id)');
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+        $this->addSql('ALTER TABLE klanten DROP partner_id');
+
+    }
+}
