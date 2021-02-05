c9d414c55ed4a70f07e4dca1d3d18be169965112
J.T. Borger <jan@pinna.nl>
2021-02-05 19:23:12 +0100

    cascade deelnames bij verwijderen van inloop vrijwilliger.

---
+++ app/DoctrineMigrations/Version20210205181912.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210205181912 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+
+        $this->addSql('ALTER TABLE `inloop_deelnames` DROP FOREIGN KEY `FK_CFB194F3B280D297`; ALTER TABLE `inloop_deelnames` ADD CONSTRAINT `FK_CFB194F3B280D297` FOREIGN KEY (`inloop_vrijwilliger_id`) REFERENCES `inloop_vrijwilligers`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    }
+}
