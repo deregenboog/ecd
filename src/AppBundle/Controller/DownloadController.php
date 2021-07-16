0a7d60a99617b3ed46a4aa7856f47ac6328ebaed
J.T. Borger <jan@pinna.nl>
2021-07-16 14:50:08 +0200

    bump version to v2.3.17

---
+++ src/AppBundle/Controller/DownloadController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Document;
+use AppBundle\Entity\Overeenkomst;
+use AppBundle\Entity\Toestemmingsformulier;
+use AppBundle\Entity\Vog;
+use AppBundle\Form\DocumentType;
+use AppBundle\Service\DocumentDaoInterface;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/documenten")
+ */
+class DocumentenController extends AbstractChildController
+{
+    protected $title = 'Documenten';
+    protected $entityName = 'Document';
+    protected $entityClass = Document::class;
+    protected $formClass = DocumentType::class;
+    protected $addMethod = 'addDocument';
+    protected $baseRouteName = 'app_documenten_';
+
+    /**
+     * @var DocumentDaoInterface
+     *
+     * @DI\Inject("AppBundle\Service\DocumentDao")
+     */
+    protected $dao;
+
+    /**
+     * @var \ArrayObject
+     *
+     * @DI\Inject("app.document.entities")
+     */
+    protected $entities;
+
+    /**
+     * @Route("/download/{filename}")
+     */
+    public function downloadAction($filename)
+    {
+        $document = $this->dao->findByFilename($filename);
+
+        $downloadHandler = $this->get('vich_uploader.download_handler');
+
+        return $downloadHandler->downloadObject($document, 'file');
+    }
+
+
+    public function createEntity($parentEntity = null)
+    {
+        switch ($this->getRequest()->get('type')) {
+            case 'vog':
+                $this->addMethod = 'setVog';
+
+                return new Vog();
+            case 'overeenkomst':
+                $this->addMethod = 'setOvereenkomst';
+
+                return new Overeenkomst();
+            case 'toestemming':
+                $this->addMethod = 'setToestemmingsformulier';
+
+                return new Toestemmingsformulier();
+            default:
+
+                return new Document();
+        }
+    }
+}
