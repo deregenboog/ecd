33f98da4d531cc9b2ab0488b867a526cba3eda25
J.T. Borger <jan@pinna.nl>
2021-08-27 10:31:09 +0200

    #1124 Aanpassing VWers MW en TW

--- src/OdpBundle/Form/VrijwilligerType.php
+++ src/OdpBundle/Form/VrijwilligerType.php
@@ -5,6 +5,7 @@ namespace OdpBundle\Form;
 use InloopBundle\Form\VrijwilligerTypeAbstract;
 use OdpBundle\Entity\Vrijwilliger;
 use OdpBundle\OdpBundle;
+use Symfony\Component\Form\FormBuilderInterface;
 
 
 class VrijwilligerType extends VrijwilligerTypeAbstract
@@ -12,4 +13,15 @@ class VrijwilligerType extends VrijwilligerTypeAbstract
     protected $dataClass = Vrijwilliger::class;
     protected $locatieSelectClass = LocatieSelectType::class;
 
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->remove("tweedeFase");
+        $builder->remove("startDatum");
+        $builder->remove("medewerkerLocatie");
+    }
 }
