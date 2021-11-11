405cf7ae10164d8b8c9b32fc88c305d495f99d27
J.T. Borger <jan@pinna.nl>
2021-11-11 17:37:32 +0100

    Wachtlijst economisch daklozen en T6 uit elkaar gehaald met fatsoenlijke sortering...

--- src/MwBundle/Service/IntakeDao.php
+++ src/MwBundle/Service/IntakeDao.php
@@ -4,6 +4,7 @@ namespace MwBundle\Service;
 
 use AppBundle\Doctrine\SqlExtractor;
 use AppBundle\Entity\Klant;
+use AppBundle\Exception\AppException;
 use AppBundle\Filter\FilterInterface;
 use AppBundle\Service\AbstractDao;
 use Doctrine\ORM\EntityManager;
@@ -15,14 +16,15 @@ use InloopBundle\Service\LocatieDao;
 use InloopBundle\Service\LocatieDaoInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use MwBundle\Entity\Verslag;
+use MwBundle\Exception\MwException;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 class IntakeDao extends AbstractDao implements IntakeDaoInterface
 {
     protected $paginationOptions = [
-        'defaultSortFieldName' => 'verslag.datum',
-        'defaultSortDirection' => 'DESC',
+//        'defaultSortFieldName' => 'verslag.datum',
+        'defaultSortDirection' => 'ASC',
         'sortFieldWhitelist' => [
             'intake.intakedatum',
             'klant.id',
@@ -45,6 +47,8 @@ class IntakeDao extends AbstractDao implements IntakeDaoInterface
     protected $alias = 'intake';
 
     protected $wachtlijstLocaties = array();
+
+    protected $actualWachtlijstLocaties = array();
     /**
      * @var EventDispatcherInterface
      */
@@ -67,87 +71,47 @@ class IntakeDao extends AbstractDao implements IntakeDaoInterface
 
     public function findAll($page = null, FilterInterface $filter = null)
     {
-        /*
-         * Onderstaande query is de basis; maar neit mogelijk in doctrine met QB.
-         * Om het niet ingewikkelder te maken dan nodig eerst de losse klant ids ophalen.
-         * En dan later de boel maar in elkaar frotten...
-         *
+        //volgende keer mee verder.
 
-        SELECT k.id, ki.klant_id, kv.klant_id FROM klanten k
-    LEFT JOIN
-    (
-        SELECT i.klant_id FROM intakes AS i
-                 LEFT JOIN intakes AS i2
-                           ON i.klant_id = i2.klant_id
-    AND (i.created > i2.created
-        OR (i.created = i2.created AND i.id > i2.id))
-        WHERE i2.klant_id IS NULL
-    AND i.locatie2_id IN (49, 50)
-    ) AS ki ON k.id = ki.klant_id
-    LEFT JOIN
-    (
-        SELECT v.klant_id FROM verslagen v
-        WHERE v.locatie_id IN (50)
-        ) AS kv ON k.id = kv.klant_id
-WHERE (ki.klant_id IS NOT NULL OR kv.klant_id IS NOT NULL)
-         */
-
-        //niet nodig omdat eerste intake en laatste intake al dmv denormalisatie zijn toegevoegd en dus te verkrijgen zijn
-//        $subQuery1 = $this->repository->createQueryBuilder("intake")
-//            ->select("intake")
-//            ->leftJoin(Intake::class,"i2",Join::WITH,"intake.klant = i2.klant
-//            AND intake.created > i2.created
-//            OR (intake.created = i2.created AND intake.id > i2.id)")
-//            ->where("i2.klant IS NULL")
-//            ->andWhere("intake.intakelocatie IN (49,50)")
-//            ;
-//
-////        $sq1 = $subQuery1->getQuery();
-////        $sql = SqlExtractor::getFullSQL($sq1);
-//        $result = $subQuery1->getQuery()->getResult();
-//        foreach($result as $r)
-//        {
-//            $ikIds[] = $r->getKlant()->getId();
-//            $iIds = $r->getId();
-//        }
+//        if(null==$filter || $filter->) return; //geen wachtlijst gelecteerd
+//       if(null==$filter || !$filter->locatie) throw new AppException("asdf");
 
 
-        $subQuery2 = $this->repository->createQueryBuilder("verslag")
-            ->resetDQLPart("select")
-
-            ->select("DISTINCT verslag.id")
-            ->resetDQLPart("from")
-            ->from(Verslag::class,"verslag")
-//            ->select("verslag")
-            ->leftJoin(Verslag::class,"v2",Join::WITH,"verslag.klant = v2.klant
-            AND verslag.created > v2.created
-            OR (verslag.created = v2.created AND verslag.id < v2.id) AND v2.klant IS NULL")
-            ->leftJoin("verslag.locatie","locatie")
-            ->leftJoin("verslag.klant","klant")
-            ->leftJoin("klant.werkgebied","werkgebied")
-//            ->where("v2.klant IS NULL")
-            ->andWhere("locatie.naam IN (:wachtlijstLocaties)")
-            ->andWhere("klant.eersteIntake IS NULL")
-            ->groupBy("verslag.klant")
-            ->setParameter("wachtlijstLocaties",$this->wachtlijstLocaties)
-        ;
-
-        if($filter !== null)
+        if($filter && $filter->locatie)
         {
-//            $filter->applyTo($subQuery2);
+            switch($filter->locatie->getNaam())
+            {
+                case 'Wachtlijst Economisch Daklozen':
+                    $this->actualWachtlijstLocaties = ['Wachtlijst Economisch Daklozen'];
+                    $this->paginationOptions['defaultSortFieldName'] = 'verslag.datum';
+                    $builder = $this->getEconomischDaklozen();
+                    break;
+                case 'Wachtlijst T6':
+                    $this->actualWachtlijstLocaties = ['Wachtlijst T6'];
+                    $builder = $this->getT6();
+                    break;
+
+            }
         }
-
-//        $sq2 = $subQuery2->getQuery();
-//        $sql = SqlExtractor::getFullSQL($subQuery2->getQuery());
-        $result = $subQuery2->getQuery()->getResult();
-
-        $vIds = array();
-        foreach($result as $r)
+        else
         {
-            $vIds[] = $r['id'];
+            $builder = $this->getDefault();
         }
 
 
+
+//        $builder = $this->repository->createQueryBuilder("verslag");
+        if(null !== $filter) $filter->applyTo($builder);
+//        $q = $builder->getQuery();
+//        $sql = SqlExtractor::getFullSQL($q);
+
+
+        return parent::doFindAll($builder, $page, $filter);
+
+    }
+
+    private function getDefault()
+    {
         $builder = $this->repository->createQueryBuilder("klant")
             ->resetDQLPart("from")
             ->from(Klant::class,"klant")
@@ -161,20 +125,58 @@ WHERE (ki.klant_id IS NOT NULL OR kv.klant_id IS NOT NULL)
             ->leftJoin("klant.verslagen","verslag")
 //            ->leftJoin("verslag.locatie","verslaglocatie")
             ->where("intakelocatie.naam IN (:wachtlijstLocaties)")
-            ->orWhere("verslag.id IN(:verslagIds) AND intake.id IS NULL")
-            ->setParameter("wachtlijstLocaties",$this->wachtlijstLocaties)
-            ->setParameter("verslagIds",$vIds)
+//            ->orWhere("verslag.id IN(:verslagIds) AND intake.id IS NULL")
+            ->setParameter("wachtlijstLocaties",$this->actualWachtlijstLocaties)
+//            ->setParameter("verslagIds",$vIds)
             ->groupBy("klant.id")
 //            ->orderBy("verslag.datum","DESC")
-
-
-
-            ;
-       if(null !== $filter) $filter->applyTo($builder);
+        ;
+//       if(null !== $filter) $filter->applyTo($builder);
 //        $q = $builder->getQuery();
 //        $sql = SqlExtractor::getFullSQL($q);
+        return $builder;
+    }
 
-        return parent::doFindAll($builder, $page, $filter);
+    private function getEconomischDaklozen()
+    {
+
+        //poging 4 :(
+//SELECT v2.id, v2.klant_id, MIN(v2.datum), v2.locatie_id FROM
+//    verslagen AS v2
+//INNER JOIN klanten AS k ON k.id = v2.klant_id
+//WHERE v2.locatie_id IN (50)
+//    AND k.first_intake_id IS NULL
+//GROUP BY v2.klant_id
+//ORDER BY v2.klant_id ASC
+        $b4 = $this->repository->createQueryBuilder("verslag")
+            ->resetDQLParts(["from",'select'])
+            ->select(' verslag, klant, locatie')
+            ->addSelect('MIN(verslag.datum) AS HIDDEN minDatum')
+            ->from(Verslag::class,'verslag')
+            ->join('verslag.klant','klant')
+            ->join('verslag.locatie','locatie')
+            ->join('klant.werkgebied','werkgebied')
+            ->where('locatie.naam IN (:wachtlijstLocaties)')
+            ->andWhere('klant.eersteIntake IS NULL')
+            ->groupBy('klant')
+            ->setParameter(':wachtlijstLocaties',$this->actualWachtlijstLocaties)
+        ;
+
+
+
+        $sql = $b4->getQuery()->getSQL();
+        return $b4;
+    }
+
+    private function getT6()
+    {
+
+
+
+        $builder = $this->getDefault();
+        return $builder;
+
+
     }
 
     /**
