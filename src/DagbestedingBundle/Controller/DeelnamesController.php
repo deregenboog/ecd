2e41da4eb7ad577ce1de14c5fe1918fb817c8883
J.T. Borger <jan@pinna.nl>
2022-03-18 09:45:22 +0100

    #1130 SCIP --> Dagbesteding

---
+++ src/DagbestedingBundle/Controller/DeelnamesController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DagbestedingBundle\Controller;
+
+use AppBundle\Controller\AbstractChildController;
+use AppBundle\Export\ExportInterface;
+use JMS\DiExtraBundle\Annotation as DI;
+use DagbestedingBundle\Entity\Deelname;
+use DagbestedingBundle\Entity\Deelnemer;
+use DagbestedingBundle\Entity\Project;
+use DagbestedingBundle\Form\DeelnameType;
+use DagbestedingBundle\Service\DeelnameDaoInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/deelnames")
+ * @Template
+ */
+class DeelnamesController extends AbstractChildController
+{
+    protected $title = 'Deelnames';
+    protected $entityName = 'deelname';
+    protected $entityClass = Deelname::class;
+    protected $formClass = DeelnameType::class;
+    protected $addMethod = 'addDeelname';
+    protected $baseRouteName = 'dagbesteding_deelnames_';
+    protected $disabledActions = ['index', 'view'];
+
+    /**
+     * @var DeelnameDaoInterface
+     *
+     * @DI\Inject("DagbestedingBundle\Service\DeelnameDao")
+     */
+    protected $dao;
+
+    /**
+     * @var \ArrayObject
+     *
+     * @DI\Inject("dagbesteding.deelname.entities")
+     */
+    protected $entities;
+
+    /**
+     * @var ExportInterface
+     *
+     * @DI\Inject("dagbesteding.export.deelnames")
+     */
+    protected $export;
+
+    /**
+     * @Route("/")
+     */
+    public function downloadAction()
+    {
+        ini_set('memory_limit', '512M');
+
+        $filename = $this->getDownloadFilename();
+
+        if ($this->isGranted('ROLE_SCIP_BEHEER')) {
+            $collection = $this->dao->findAll();
+        } else {
+            $collection = $this->dao->findByMedewerker($this->getMedewerker());
+        }
+
+        return $this->export->create($collection)->getResponse($filename);
+    }
+
+    protected function createEntity($parentEntity = null)
+    {
+        $deelnemer = $parentEntity instanceof Deelnemer ? $parentEntity : null;
+        $project = $parentEntity instanceof Project ? $parentEntity : null;
+
+        return new Deelname($deelnemer, $project);
+    }
+}
---
+++ src/DagbestedingBundle/Entity/Werkdoel.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace DagbestedingBundle\Entity;
+
+use AppBundle\Entity\Medewerker;
+use AppBundle\Model\IdentifiableTrait;
+use AppBundle\Model\MedewerkerSubjectInterface;
+use AppBundle\Model\TimestampableTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="dagbesteding_werkdoelen")
+ * @ORM\HasLifecycleCallbacks
+ * @Gedmo\Loggable
+ */
+class Werkdoel implements MedewerkerSubjectInterface
+{
+    use IdentifiableTrait, TimestampableTrait;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     * @Gedmo\Versioned
+     */
+    protected $tekst;
+
+    /**
+     * @var Deelnemer
+     *
+     * @ORM\ManyToOne(targetEntity="Deelnemer", inversedBy="werkdoelen")
+     * @Gedmo\Versioned
+     */
+    protected $deelnemer;
+
+    /**
+     * @var Traject
+     *
+     * @ORM\ManyToOne(targetEntity="DagbestedingBundle\Entity\Traject", inversedBy="werkdoelen")
+     * @Gedmo\Versioned
+     */
+    protected $traject;
+
+    /**
+     * @var Medewerker
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Medewerker")
+     * @Gedmo\Versioned
+     */
+    protected $medewerker;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="date")
+     * @Gedmo\Versioned
+     */
+    protected $datum;
+
+    public function __construct()
+    {
+        $this->datum = new \DateTime();
+    }
+
+    public function getDeelnemer()
+    {
+        return $this->deelnemer;
+    }
+
+    public function getMedewerker()
+    {
+        return $this->medewerker;
+    }
+
+    public function setMedewerker(Medewerker $medewerker)
+    {
+        $this->medewerker = $medewerker;
+
+        return $this;
+    }
+
+    public function setDeelnemer(Deelnemer $deelnemer)
+    {
+        $this->deelnemer = $deelnemer;
+
+        return $this;
+    }
+
+    public function getTekst()
+    {
+        return $this->tekst;
+    }
+
+    public function setTekst($tekst)
+    {
+        $this->tekst = $tekst;
+
+        return $this;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDatum()
+    {
+        return $this->datum;
+    }
+
+    /**
+     * @param \DateTime $datum
+     */
+    public function setDatum(\DateTime $datum)
+    {
+        $this->datum = $datum;
+
+        return $this;
+    }
+
+    /**
+     * @return Traject
+     */
+    public function getTraject(): Traject
+    {
+        return $this->traject;
+    }
+
+    /**
+     * @param Traject $traject
+     * @return Werkdoel
+     */
+    public function setTraject(Traject $traject): Werkdoel
+    {
+        $this->traject = $traject;
+        return $this;
+    }
+
+
+}
