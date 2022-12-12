19d7c595531148598133567c6c0aa6f94c909eb6
Bart Huttinga <mail@barthuttinga.nl>
2022-12-12 15:00:34 +0100

    Fix sorteren trajectcoaches

---
+++ tests/DagbestedingBundle/Controller/TrajectcoachesControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\DagbestedingBundle\Controller;
+
+use AppBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class TrajectcoachesControllerTest extends WebTestCase
+{
+    public function testSortColumns()
+    {
+        $medewerker = $this->getContainer()->get(\AppBundle\Service\MedewerkerDao::class)->findByUsername('dagbesteding_admin');
+        $this->logIn($medewerker);
+
+        $crawler = $this->client->request('GET', '/dagbesteding/trajectcoaches/');
+        $this->assertStatusCode(200, $this->client);
+
+        $headers = $crawler->filter('tr th a.sortable');
+        $this->assertGreaterThan(1, $headers->count());
+
+        $headers->each(function ($header) {
+            // @see https://github.com/KnpLabs/knp-components/issues/160
+            $request = Request::create($header->link()->getUri());
+            $_GET = $request->query->all();
+
+            $this->client->click($header->link());
+            $this->assertStatusCode(200, $this->client);
+        });
+    }
+}
