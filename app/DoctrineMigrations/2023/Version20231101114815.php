813e84d668729b8f47b83c6b0688c7efaf8892c3
jtborger <jan@pinna.nl>
2023-11-01 12:54:55 +0100

    Adding locatieType nachtopvang in a migration...

---
+++ app/DoctrineMigrations/2023/Version20231101114815.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231101114815 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("INSERT INTO locatie_type (naam) VALUES ('Nachtopvang')");
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
