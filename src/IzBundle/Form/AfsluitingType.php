81a1f685edbd1fe79b7ce1cee7635f920dc63bf9
Bart Huttinga <mail@barthuttinga.nl>
2024-04-17 16:32:18 +0200

    Onderscheid maken afsluitingen deelnemers/vrijwilligers #1160

--- src/IzBundle/Form/AfsluitingType.php
+++ src/IzBundle/Form/AfsluitingType.php
@@ -5,6 +5,7 @@ namespace IzBundle\Form;
 use AppBundle\Form\BaseType;
 use IzBundle\Entity\Afsluiting;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,6 +19,11 @@ class AfsluitingType extends AbstractType
     {
         $builder
             ->add('naam')
+            ->add('doelgroepen', ChoiceType::class, [
+                'choices' => array_flip(Afsluiting::DOELGROEPEN_LABELS),
+                'multiple' => true,
+                'by_reference' => false,
+            ])
             ->add('actief')
             ->add('submit', SubmitType::class)
         ;
