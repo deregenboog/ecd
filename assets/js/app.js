d1097940c10a3f91541a871a7abcc9aa3327a83e
Bart Huttinga <mail@barthuttinga.nl>
2024-04-18 11:05:27 +0200

    Remove explicit jQuery/Popper import

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,11 @@
-// 3rd party javascript
-import 'jquery';
-import 'lodash';
+// 3rd party javascript and styles
 import 'bootstrap';
-import 'popper.js';
-import 'select2';
-
-// 3rd party styles
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'lodash';
+import 'select2';
 import 'select2/dist/css/select2.min.css';
 
-// App specific javascript
-global.$ = $; // make jQuery globally available as $
+// app specific javascript and styles
 import './global.js';
-
-// App specific styles
 import '../css/regenboog-bootstrap.css';
 import '../css/spacing.css';
