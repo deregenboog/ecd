65f108d8cf76b35536f473c2dbbcee893a039cb3
jtborger <jan@pinna.nl>
2024-08-27 20:38:30 +0200

    Change docker config so OPCache is enabled.

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
+
 umask(0002); //https://symfony.com/doc/5.4/setup/file_permissions.html#3-without-using-acl
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
