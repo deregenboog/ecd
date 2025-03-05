5b5d87e167a7c37e252915a7f9b4bbbe23b4351c
kevin <kevin@toomba.nl>
2025-03-05 10:52:32 +0100

    fixed small bug in check for number of dates

--- src/VillaBundle/Controller/OvernachtingenController.php
+++ src/VillaBundle/Controller/OvernachtingenController.php
@@ -83,10 +83,10 @@ class OvernachtingenController extends AbstractController
             return new JsonResponse(['success' => true, 'message' => 'Entity created successfully'], Response::HTTP_OK);
         }
         $saldo = $slaper->calculateSaldo();
-        if(is_numeric($saldo) & ($saldo >= 0 && $saldo < 4)){
+        if(is_numeric($saldo) & ($saldo > 0 && $saldo < 4)){
             $this->addFlash('warning', 'Let op, deze slaper heeft nog '.$saldo.' overnachtingen.');
         }
-        if(is_numeric($saldo) & $saldo < 0 ){
+        if(is_numeric($saldo) & $saldo <= 0 ){
             $this->addFlash('danger', 'Let op, deze slaper heeft geen overnachtingen meer beschikbaar.');
         }
         $formHtml = $this->renderView('edit.ajax.html.twig', ['form' => $form->createView()]);
