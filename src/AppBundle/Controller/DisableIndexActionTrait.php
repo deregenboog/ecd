4bc76863e6943dc6d7308e1fc6751aff19938949
jtborger <jan@pinna.nl>
2025-06-06 10:37:05 +0200

    Documentatie toegevoegd voor DisableIndexActionTrait.php

--- src/AppBundle/Controller/DisableIndexActionTrait.php
+++ src/AppBundle/Controller/DisableIndexActionTrait.php
@@ -6,6 +6,21 @@ use Symfony\Component\HttpFoundation\Request;
 
 trait DisableIndexActionTrait
 {
+    /**
+     * Het idee achter deze trait en het overriden van de indexAction komt voor uit de tests/AppBundle/Controller/ControllerTest
+     * Deze gaat namelijk alle beschikbare index routes af om te kijken of die een tabel teruggeven.
+     * Dit is handig. Maar, niet alle controllers hoeven een index route te hebben.
+     * Daar is deze trait voor.
+     *
+     * Het was mooier geweest om die controllers te laten overerven van een nieuwe Abstract controller:
+     * NoIndexAbstractController, waarin dit is dan geregeld wordt. Dat is veel transparanter.
+     *
+     * @todo Alle controllers die deze trait gbruiken laten overerven van (nieuw te maken) NoIndexAbstractController.
+     * Evt kan die NoIndexAbstractController natuurlijk deze trait gebruiken ;)
+     *
+     * @param Request $request
+     * @return void
+     */
     public function indexAction(Request $request)
     {
     }
