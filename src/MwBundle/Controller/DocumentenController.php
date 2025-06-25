8254930757fb5089d4a5d491d092823510e86079
kevin <kevin@toomba.nl>
2025-06-25 21:24:59 +0200

    Multi upload met limiet toegevoegd IZ

--- src/MwBundle/Controller/DocumentenController.php
+++ src/MwBundle/Controller/DocumentenController.php
@@ -5,6 +5,9 @@ namespace MwBundle\Controller;
 use AppBundle\Controller\AbstractChildController;
 use AppBundle\Controller\DisableIndexActionTrait;
 use AppBundle\Entity\Klant;
+use AppBundle\Exception\AppException;
+use AppBundle\Exception\UserException;
+use AppBundle\Model\MedewerkerSubjectInterface;
 use MwBundle\Entity\Document;
 use MwBundle\Entity\Vrijwilliger;
 use MwBundle\Form\DocumentType;
@@ -12,6 +15,7 @@ use MwBundle\Service\DocumentDaoInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 /**
  * @Route("/documenten")
@@ -46,19 +50,121 @@ class DocumentenController extends AbstractChildController
 
     /**
      * @Route("/add")
-     *
+     * 
      * @Template
      */
     public function addAction(Request $request)
     {
         [$parentEntity, $this->parentDao] = $this->getParentConfig($request);
+
         if ($parentEntity instanceof Klant) {
             $this->addMethod = null;
-        } elseif ($parentEntity instanceof Vrijwilliger) {
-            $this->addMethod = 'addDocument';
         }
 
-        return parent::addAction($request);
+        if (!$this->addMethod && !$this->allowEmpty) {
+            throw new \RuntimeException('Property $addMethod must be set in class '.get_class($this));
+        }
+        if (!$parentEntity && !$this->allowEmpty) {
+            throw new AppException(sprintf('Kan geen %s aan deze entiteit toevoegen. Extra data: \n queryString: %s \nUser: %s', $this->entityName, $request->getQueryString(), (string) $request->getUserInfo()));
+        }
+
+        $temporaryEntityForForm = new $this->entityClass();
+        if (null !== $parentEntity) {
+            $parentClassShortName = (new \ReflectionClass($parentEntity))->getShortName();
+            $setParentMethod = 'set'.ucfirst($parentClassShortName);
+            if (method_exists($temporaryEntityForForm, $setParentMethod)) {
+                $temporaryEntityForForm->{$setParentMethod}($parentEntity);
+            }
+        }
+
+        $maxFilesFromRequest = $request->query->getInt('max_files');
+
+        $currentFormOptions = array_merge($this->formOptions, [
+            'medewerker' => $this->getMedewerker(),
+        ]);
+
+        if (null !== $maxFilesFromRequest && $maxFilesFromRequest > 0) {
+            $currentFormOptions['max_files'] = $maxFilesFromRequest;
+        }
+
+        $form = $this->getForm($this->formClass, $temporaryEntityForForm, $currentFormOptions);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFiles = $form->get('documenten')->getData();
+
+            $selectedMedewerker = $form->get('medewerker')->getData();
+            $entitiesCreatedCount = 0;
+
+            if (is_array($uploadedFiles) && count($uploadedFiles) > 0) {
+                foreach ($uploadedFiles as $uploadedFile) {
+                    $documentEntity = new $this->entityClass();
+                    $documentEntity->setFile($uploadedFile);
+
+                    $documentEntity->setNaam($uploadedFile->getClientOriginalName());
+                    if ($selectedMedewerker instanceof \AppBundle\Entity\Medewerker) {
+                        $documentEntity->setMedewerker($selectedMedewerker);
+                    } else {
+                        $this->logger->error('Poging tot opslaan document zonder geldige medewerker. Dit is niet toegestaan en zou door formuliervalidatie voorkomen moeten worden.', ['document_name' => $uploadedFile->getClientOriginalName(), 'selected_medewerker_type' => is_object($selectedMedewerker) ? get_class($selectedMedewerker) : gettype($selectedMedewerker)]);
+                        $this->addFlash('danger', "Fout bij opslaan bestand {$uploadedFile->getClientOriginalName()}: Medewerker is niet (correct) geselecteerd/opgegeven. Document niet opgeslagen.");
+                        continue;
+                    }
+
+                    if (null !== $parentEntity && $this->addMethod && method_exists($parentEntity, $this->addMethod)) {
+
+                        $parentEntity->{$this->addMethod}($documentEntity);
+                    } elseif (null !== $parentEntity && $parentEntity instanceof Klant) {
+                        if (method_exists($documentEntity, 'setKlant')) {
+                            $documentEntity->setKlant($parentEntity);
+                        } else {
+                            $this->logger->error('Document entity is missing setKlant method, cannot associate with Klant.', ['parent_id' => $parentEntity->getId()]);
+                            $this->addFlash('danger', "Fout bij opslaan bestand {$uploadedFile->getClientOriginalName()}: Interne configuratiefout, document kon niet aan klant gekoppeld worden.");
+                            continue;
+                        }
+                    }
+
+                    try {
+                        $this->beforeCreate($documentEntity);
+
+                        if ((!$parentEntity && $this->allowEmpty) || ($parentEntity instanceof Klant)) {
+                            $this->dao->create($documentEntity);
+                        }
+                        $this->afterCreate($documentEntity);
+                        $entitiesCreatedCount++;
+                    } catch (UserException $e) {
+                        $this->addFlash('danger', "Fout bij opslaan bestand {$uploadedFile->getClientOriginalName()}: ".$e->getMessage());
+                    } catch (\Exception $e) {
+                        $this->logger->error("Error persisting document {$uploadedFile->getClientOriginalName()}: ".$e->getMessage(), ['exception' => $e]);
+                        $this->addFlash('danger', "Fout bij opslaan bestand {$uploadedFile->getClientOriginalName()}: ".$e->getMessage());
+                    }
+                }
+
+                if ($parentEntity && $entitiesCreatedCount > 0) {
+                    if (!$parentEntity instanceof Klant) {
+                        $this->parentDao->update($parentEntity);
+                    }
+                }
+
+                if ($entitiesCreatedCount > 0) {
+                    $this->addFlash('success', ucfirst($this->entityName).($entitiesCreatedCount > 1 ? 'en zijn' : ' is').' toegevoegd.');
+                }
+            } else {
+                $this->addFlash('warning', 'Geen documenten geselecteerd of er is een fout opgetreden bij het uploaden.');
+            }
+
+            return $this->afterFormSubmitted($request, $parentEntity ?: $temporaryEntityForForm, $form);
+        }
+
+        $actualMaxFilesForTemplate = $form->getConfig()->getOption('max_files');
+
+        return array_merge([
+            'entity' => $temporaryEntityForForm,
+            'parent_entity' => $parentEntity,
+            'form' => $form->createView(),
+            'max_files' => $actualMaxFilesForTemplate,
+        ], $this->addParams($temporaryEntityForForm, $request));
+
+        
     }
 
     protected function createEntity($parentEntity = null)
