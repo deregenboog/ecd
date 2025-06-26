03b797493d6aba0ca816e9c718ff987e7c1466f9
kevin <kevin@toomba.nl>
2025-06-26 14:36:54 +0200

    start

---
+++ src/OekraineBundle/Entity/AfsluitredenBezoeker.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace OekraineBundle\Entity;
+
+use AppBundle\Model\ActivatableTrait;
+use AppBundle\Model\IdentifiableTrait;
+use AppBundle\Model\NameableTrait;
+use AppBundle\Model\NotDeletableTrait;
+use AppBundle\Model\TimestampableTrait;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity
+ *
+ * @ORM\Table("oekraine_afsluitredenen_bezoekers")
+ *
+ * @ORM\HasLifecycleCallbacks
+ *
+ * @Gedmo\Loggable
+ */
+class AfsluitredenBezoeker
+{
+    use IdentifiableTrait;
+    use NameableTrait;
+    use ActivatableTrait;
+    use TimestampableTrait;
+    use NotDeletableTrait;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     *
+     * @Gedmo\Versioned
+     */
+    protected $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     *
+     * @Gedmo\Versioned
+     */
+    protected $modified;
+}
